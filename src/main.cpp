643a079fcd8f427d4e5ef7e53e28427106346e15
Gabriel Calper Seabra <gabrielseabra@alunos.utfpr.edu.br>
2021-12-08 17:16:59 -0400

    update

--- src/main.cpp
+++ src/main.cpp
@@ -11,30 +11,50 @@
 
 
 #define DHTPIN 14        				// Pino do DHT 11 em D5
+#define PluviINTERRUPT 12				// D6 - Pino de interrupção da báscula
+#define pinoChuva 13 					//D7
 #define DHTTYPE DHT11					// Tipo do produto
-#define PluviINTERRUPT 13 				// D7 - Pino de interrupção da báscula
-#define pinoChuva 14 					// D5 Pino digital do sensor de chuva
+
+
+
 DHT dht(DHTPIN,DHTTYPE);
 DHTinfo dhtinfo;
 SensorRain sensorrain;
 Magnetometer magnetometer;
 // Enter network credentials:
-const char* ssid     = "Dalzira";
-const char* password = "96261194";
-//Define pinos
+const char* ssid     = "JS";
+const char* password = "dhcp8hgkg";
+
 float pulsePluvi = 0;
+
+
 //MPU6050
-const int MPU = 0x68;
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int minVal=265;
+int maxVal=402;
+double x;
+double y;
+double z;
 
 
-int AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+void ICACHE_RAM_ATTR interrupcao()
+{
+	Serial.println("Interrupcao");
+	Serial.println(pulsePluvi);
 
-
+	if(digitalRead(pinoChuva) == LOW)
+	{
+		Serial.println("Choveu");
+		pulsePluvi++;
+	}
+	delayMicroseconds(100);
+}
 
 void Gyro(void)
 {
-	Wire.begin();
-	Wire.beginTransmission(MPU);
+	Wire.begin(D1,D2);
+	Wire.beginTransmission(MPU_addr);
 	Wire.write(0x6B);
 	Wire.write(0);
 	Wire.endTransmission(true);
@@ -43,29 +63,34 @@ void Gyro(void)
 
 void Gyro_Read(void)
 {
-	Wire.beginTransmission(MPU);
+	Wire.beginTransmission(MPU_addr);
 	Wire.write(0x3B);
 	Wire.endTransmission(false);
-	Wire.requestFrom(MPU, 14, true);
-	AcX = Wire.read() <<8 | Wire.read();     
-	AcY = Wire.read() <<8 | Wire.read();
-	AcZ = Wire.read() <<8 | Wire.read();
-	Tmp = Wire.read() <<8 | Wire.read();
-	GyX = Wire.read() <<8 | Wire.read();
-	GyY = Wire.read() <<8 | Wire.read();
-	GyZ = Wire.read() <<8 | Wire.read();
-	Serial.print("AcX = "); Serial.print(AcX);
-	Serial.print("AcX = "); Serial.print(AcX);
-	Serial.print(" | AcZ = "); Serial.print(AcZ);
-	Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);
-	Serial.print(" | GyX = "); Serial.print(GyX);
-	Serial.print(" | GyY = "); Serial.print(GyY);
-	Serial.print(" | GyZ = "); Serial.println(GyZ);
+	Wire.requestFrom(MPU_addr,14,true);
 
+	AcX=Wire.read()<<8|Wire.read();
+	AcY=Wire.read()<<8|Wire.read();
+	AcZ=Wire.read()<<8|Wire.read();
+
+	int xAng = map(AcX,minVal,maxVal,-90,90);
+	int yAng = map(AcY,minVal,maxVal,-90,90);
+	int zAng = map(AcZ,minVal,maxVal,-90,90);
+	
+	x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+	y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+	z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+
+	Serial.print("AngleX= ");
+	Serial.println(x);
+	
+	Serial.print("AngleY= ");
+	Serial.println(y);
+	
+	Serial.print("AngleZ= ");
+	Serial.println(z);
+	Serial.println("-----------------------------------------");
+    delay(400);
 }
-
-
-
 
 DHTinfo dht_read()
 {
@@ -74,17 +99,16 @@ DHTinfo dht_read()
 	return dhtinfo;
 }
 
+
+
 void POST(void)
 {
 	WiFiClientSecure client;
 	const char* host = "script.google.com";															// Google Host
 	const char* fingerprint = "46 B2 C3 44 9C 59 09 8B 01 B6 F8 BD 4C FB 00 74 91 2F EF F6";		// Sha1
-	String GAS_ID = "AKfycbwSfoM2Bc0RX7-2aNFsUpTvurlR0Y4hFj43IndotJ4IqmVnvfVpFc6R-t7I9xZ9j3cT";     // ScriptID
+	String GAS_ID = "AKfycby_WjFzErWMlZn6jWav-_IVhY3ZGUsRxAT_tDUko-RbznWqtz-fajY141gRUbgSVHAk";     // ScriptID
 	const int httpsPort = 443;
-	int param_1 = 1;			// temperature
-	int param_2 = 2;			// humidity
-	int param_3 = 3;														// Port
-	String parameters = "teste1=" + String(dhtinfo.temperature) + "&teste2=" + String(dhtinfo.Humidity) + "&teste3=" + String(param_3);
+	String parameters = "teste_1=" + String(dhtinfo.temperature) + "&teste_2=" + String(dhtinfo.Humidity) + "&teste_3=" + String(pulsePluvi/2*0.125) + "&teste_4=" + String(x) + "&teste_5=" + String(y) + "&teste_6=" + String(z);
 	client.setInsecure();
 	if (!client.connect(host, httpsPort)) 
 	{
@@ -115,24 +139,10 @@ void POST(void)
 	}
 }
 
-void ICACHE_RAM_ATTR interrupcao()
+void wifi()
 {
-	if(pinoChuva == LOW)
-	{
-		pulsePluvi++;
-	}
-	delayMicroseconds(100);
-}
-
-void setup() 
-{
-	Gyro();
-	attachInterrupt(digitalPinToInterrupt(PluviINTERRUPT), interrupcao, FALLING); //Interrupção
-	bool ret;
-	bool flag = false;
 	Serial.begin(9600);				//Baud Rate UART
-	dht.begin();					//Start dht
-	ret = WiFi.begin(ssid, password);
+	WiFi.begin(ssid, password);
 	Serial.println("Connecting");
 	while (WiFi.status() != WL_CONNECTED) 
 	{
@@ -140,6 +150,19 @@ void setup()
 		Serial.print(".");
 	}
 }
+void pluvi()
+{
+	attachInterrupt(digitalPinToInterrupt(PluviINTERRUPT), interrupcao, FALLING);
+}
+
+void setup() 
+{
+	wifi();
+	dht.begin();
+	Gyro();
+	pluvi();
+}
+
 void loop() 
 {
 	bool itsRaining = false;
@@ -148,7 +171,11 @@ void loop()
 	{
 		Serial.println("Erro Leitura");
 	}
-	
+	else
+	{
+		Serial.println(dhtinfo.temperature);
+		Serial.println(dhtinfo.Humidity);
+	}
 	if (digitalRead(pinoChuva) == LOW)
 	{
 		itsRaining = true; 
